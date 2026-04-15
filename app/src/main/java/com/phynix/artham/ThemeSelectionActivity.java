869cd0b59f4d25fe1b4f11677ca8a259790b81ae
Satwik Bardhan <codewithsatwik06@gmail.com>
2026-04-15 22:28:06 +0530

    Bug fixes and Performance Improvements

--- app/src/main/java/com/phynix/artham/ThemeSelectionActivity.java
+++ app/src/main/java/com/phynix/artham/ThemeSelectionActivity.java
@@ -16,8 +16,8 @@ import com.phynix.artham.utils.ThemeManager;
 
 public class ThemeSelectionActivity extends AppCompatActivity {
 
-    private CardView cardLight, cardDark, cardPurple;
-    private RadioButton radioLight, radioDark, radioPurple;
+    private CardView cardSystem, cardLight, cardDark, cardPurple, cardEmerald, cardRose;
+    private RadioButton radioSystem, radioLight, radioDark, radioPurple, radioEmerald, radioRose;
     private ImageView backButton;
 
     @Override
@@ -37,34 +37,59 @@ public class ThemeSelectionActivity extends AppCompatActivity {
 
     private void initViews() {
         backButton = findViewById(R.id.back_button);
+        cardSystem = findViewById(R.id.cardSystem);
         cardLight = findViewById(R.id.cardLight);
         cardDark = findViewById(R.id.cardDark);
         cardPurple = findViewById(R.id.cardPurple);
+        cardEmerald = findViewById(R.id.cardEmerald);
+        cardRose = findViewById(R.id.cardRose);
+        radioSystem = findViewById(R.id.radioSystem);
         radioLight = findViewById(R.id.radioLight);
         radioDark = findViewById(R.id.radioDark);
         radioPurple = findViewById(R.id.radioPurple);
+        radioEmerald = findViewById(R.id.radioEmerald);
+        radioRose = findViewById(R.id.radioRose);
     }
 
     private void setupCurrentState() {
         String currentTheme = ThemeManager.getTheme(this);
+        radioSystem.setChecked(false);
         radioLight.setChecked(false);
         radioDark.setChecked(false);
         radioPurple.setChecked(false);
+        radioEmerald.setChecked(false);
+        radioRose.setChecked(false);
 
-        if (ThemeManager.THEME_LIGHT.equals(currentTheme)) {
-            radioLight.setChecked(true);
-        } else if (ThemeManager.THEME_PURPLE.equals(currentTheme)) {
-            radioPurple.setChecked(true);
-        } else {
-            radioDark.setChecked(true);
+        switch (currentTheme) {
+            case ThemeManager.THEME_LIGHT:
+                radioLight.setChecked(true);
+                break;
+            case ThemeManager.THEME_DARK:
+                radioDark.setChecked(true);
+                break;
+            case ThemeManager.THEME_PURPLE:
+                radioPurple.setChecked(true);
+                break;
+            case ThemeManager.THEME_EMERALD:
+                radioEmerald.setChecked(true);
+                break;
+            case ThemeManager.THEME_ROSE:
+                radioRose.setChecked(true);
+                break;
+            default:
+                radioSystem.setChecked(true);
+                break;
         }
     }
 
     private void setupClickListeners() {
         backButton.setOnClickListener(v -> finish());
+        cardSystem.setOnClickListener(v -> handleThemeChange(ThemeManager.THEME_SYSTEM));
         cardLight.setOnClickListener(v -> handleThemeChange(ThemeManager.THEME_LIGHT));
         cardDark.setOnClickListener(v -> handleThemeChange(ThemeManager.THEME_DARK));
         cardPurple.setOnClickListener(v -> handleThemeChange(ThemeManager.THEME_PURPLE));
+        cardEmerald.setOnClickListener(v -> handleThemeChange(ThemeManager.THEME_EMERALD));
+        cardRose.setOnClickListener(v -> handleThemeChange(ThemeManager.THEME_ROSE));
     }
 
     private void handleThemeChange(String newTheme) {
@@ -76,9 +101,12 @@ public class ThemeSelectionActivity extends AppCompatActivity {
             ThemeManager.applyTheme(newTheme);
 
             // 2. Update UI Radio Buttons
+            radioSystem.setChecked(ThemeManager.THEME_SYSTEM.equals(newTheme));
             radioLight.setChecked(ThemeManager.THEME_LIGHT.equals(newTheme));
             radioDark.setChecked(ThemeManager.THEME_DARK.equals(newTheme));
             radioPurple.setChecked(ThemeManager.THEME_PURPLE.equals(newTheme));
+            radioEmerald.setChecked(ThemeManager.THEME_EMERALD.equals(newTheme));
+            radioRose.setChecked(ThemeManager.THEME_ROSE.equals(newTheme));
 
             // 3. Restart App to apply to ALL pages
             ThemeManager.restartApp(this);
--- app/src/main/java/com/phynix/artham/utils/ThemeManager.java
+++ app/src/main/java/com/phynix/artham/utils/ThemeManager.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.res.Configuration;
 import android.os.Handler;
 import android.os.Looper;
 
@@ -13,9 +14,12 @@ import com.phynix.artham.HomePage;
 import com.phynix.artham.R;
 
 public class ThemeManager {
+    public static final String THEME_SYSTEM = "system";
     public static final String THEME_LIGHT = "light";
     public static final String THEME_DARK = "dark";
     public static final String THEME_PURPLE = "purple";
+    public static final String THEME_EMERALD = "emerald";
+    public static final String THEME_ROSE = "rose";
 
     private static final String PREF_NAME = "app_theme";
     private static final String KEY_THEME = "selected_theme";
@@ -26,17 +30,20 @@ public class ThemeManager {
      */
     public static void applyTheme(String theme) {
         switch (theme) {
+            case THEME_SYSTEM:
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM);
+                break;
             case THEME_LIGHT:
-            case THEME_PURPLE:
-                // Both Light and Purple use the "Light" mode base for resources
                 AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
                 break;
             case THEME_DARK:
+            case THEME_PURPLE:
+            case THEME_EMERALD:
+            case THEME_ROSE:
                 AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
                 break;
             default:
-                // [FIX] Default to LIGHT now
-                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM);
                 break;
         }
     }
@@ -47,16 +54,42 @@ public class ThemeManager {
      */
     public static void applyActivityTheme(Activity activity) {
         SharedPreferences prefs = activity.getSharedPreferences(PREF_NAME, Context.MODE_PRIVATE);
-        // [FIX] Default is LIGHT
-        String theme = prefs.getString(KEY_THEME, THEME_LIGHT);
+        String theme = prefs.getString(KEY_THEME, THEME_SYSTEM);
 
-        if (THEME_PURPLE.equals(theme)) {
-            activity.setTheme(R.style.Theme_Artham_Purple);
-        } else if (THEME_DARK.equals(theme)) {
-            activity.setTheme(R.style.Theme_Artham_Dark);
-        } else {
-            activity.setTheme(R.style.Theme_Artham); // Light
+        switch (theme) {
+            case THEME_SYSTEM:
+                // Follow device setting: dark system → Dark theme, light system → Light theme
+                if (isSystemDarkMode(activity)) {
+                    activity.setTheme(R.style.Theme_Artham_Dark);
+                } else {
+                    activity.setTheme(R.style.Theme_Artham);
+                }
+                break;
+            case THEME_DARK:
+                activity.setTheme(R.style.Theme_Artham_Dark);
+                break;
+            case THEME_PURPLE:
+                activity.setTheme(R.style.Theme_Artham_Purple);
+                break;
+            case THEME_EMERALD:
+                activity.setTheme(R.style.Theme_Artham_Emerald);
+                break;
+            case THEME_ROSE:
+                activity.setTheme(R.style.Theme_Artham_Rose);
+                break;
+            default:
+                activity.setTheme(R.style.Theme_Artham);
+                break;
         }
+    }
+
+    /**
+     * Checks if the device is currently in dark mode.
+     */
+    public static boolean isSystemDarkMode(Context context) {
+        int nightModeFlags = context.getResources().getConfiguration().uiMode
+                & Configuration.UI_MODE_NIGHT_MASK;
+        return nightModeFlags == Configuration.UI_MODE_NIGHT_YES;
     }
 
     public static void saveTheme(Context context, String theme) {
@@ -67,8 +100,7 @@ public class ThemeManager {
 
     public static String getTheme(Context context) {
         SharedPreferences prefs = context.getSharedPreferences(PREF_NAME, Context.MODE_PRIVATE);
-        // [FIX] Default is LIGHT
-        return prefs.getString(KEY_THEME, THEME_LIGHT);
+        return prefs.getString(KEY_THEME, THEME_SYSTEM);
     }
 
     public static void restartApp(Activity activity) {
